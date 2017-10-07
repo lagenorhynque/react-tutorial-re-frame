b754f7a1a7dc9d60ac36ec40a053bcf319a3f42a
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2017-10-08 00:49:20 +0900

    implement tutorial app with reagent

--- src/cljs/react_tutorial_re_frame/views.cljs
+++ src/cljs/react_tutorial_re_frame/views.cljs
@@ -1,7 +1,121 @@
 (ns react-tutorial-re-frame.views
-  (:require [re-frame.core :as re-frame]))
+  (:require [clojure.string :as str]
+            [re-frame.core :as re-frame]
+            [reagent.core :as reagent]))
+
+(defn square [& {:keys [value on-click win?]}]
+  [:button.square {:on-click on-click
+                   :class (when win?
+                            "win-square")}
+   value])
+
+(defn calculate-winner [squares]
+  (let [lines [[0 1 2]
+               [3 4 5]
+               [6 7 8]
+               [0 3 6]
+               [1 4 7]
+               [2 5 8]
+               [0 4 8]
+               [2 4 6]]]
+    (reduce (fn [_ [a b c :as win-ps]]
+              (when (and (squares a)
+                         (= (squares a) (squares b))
+                         (= (squares a) (squares c)))
+                (reduced [(squares a) win-ps])))
+            nil
+            lines)))
+
+(defn board [& {:keys [squares on-click win-ps]}]
+  (letfn [(render-square [i]
+            [square
+             :value (squares i)
+             :on-click #(on-click i)
+             :win? (some #(= i %) win-ps)])]
+    (into [:div]
+          (->> (range 9)
+               (map #(render-square %))
+               (partition 3)
+               (map #(into [:div.board-row] %))))))
+
+(defn game []
+  (let [state (reagent/atom {:history [{:squares (vec (repeat 9 nil))
+                                        :i nil}]
+                             :x-is-next? true
+                             :step-number 0
+                             :order-asc? true})]
+    (letfn [(handle-click [i]
+              (let [{:keys [history x-is-next? step-number]} @state
+                    history (vec (take (inc step-number) history))
+                    current (nth history (dec (count history)))
+                    squares (:squares current)]
+                (when-not (or (calculate-winner squares)
+                              (squares i))
+                  (swap! state assoc
+                         :history (conj history
+                                        {:squares (assoc
+                                                   squares
+                                                   i
+                                                   (if x-is-next? "X" "O"))
+                                         :i i})
+                         :x-is-next? (not x-is-next?)
+                         :step-number (count history)))))
+            (jump-to [step]
+              (swap! state assoc
+                     :x-is-next? (even? step)
+                     :step-number step))
+            (calculate-location [i]
+              (map (comp inc Math/floor) [(/ i 3) (mod i 3)]))
+            (flip-sort-order [order-asc?]
+              (swap! state assoc
+                     :order-asc? (not order-asc?)))]
+      (fn []
+        (let [history (:history @state)
+              selected (:step-number @state)
+              current (nth history selected)
+              [winner win-ps] (calculate-winner (:squares current))
+              status (if winner
+                       (str "Winner: " winner)
+                       (str "Next player: "
+                            (if (:x-is-next? @state) "X" "O")))
+              order-asc? (:order-asc? @state)
+              sort (fn []
+                     [:a {:href "#"
+                          :on-click #(flip-sort-order order-asc?)}
+                      (if order-asc? "↓" "↑")])
+              moves (map-indexed (fn [move {:keys [i]}]
+                                   (let [desc (if (zero? move)
+                                                (str "Game start")
+                                                (str "Move #("
+                                                     (str/join
+                                                      ", "
+                                                      (calculate-location i))
+                                                     ")"))]
+                                     [:li {:key move
+                                           :class (when (= move selected)
+                                                    "move-selected")}
+                                      [:a {:href "#"
+                                           :on-click #(jump-to move)}
+                                       desc]]))
+                                 history)]
+          [:div.game
+           [:div.game-board
+            [board
+             :squares (:squares current)
+             :on-click handle-click
+             :win-ps win-ps]]
+           [:div.game-info
+            [:div
+             status]
+            [:div
+             [sort]]
+            [:ol
+             (if order-asc?
+               moves
+               (reverse moves))]]])))))
 
 (defn main-panel []
   (let [name (re-frame/subscribe [:name])]
     (fn []
-      [:div "Hello from " @name])))
+      [:div "Hello from " @name
+       [game]])))
